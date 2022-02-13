8edd2a67e20fab859635e6a08b013bd5e9b3fea1
Crosstars <73341745+crosstars@users.noreply.github.com>
2022-02-13 11:22:43 +0530

    Add files via upload

---
+++ GUI/GUI/load_ui.py
@@ -0,0 +1,41 @@
+from PyQt5 import QtWidgets
+from UI.load import loader
+from Validation.validator import *
+import sys
+
+app=QtWidgets.QApplication(sys.argv)
+    
+ui,win = loader.load(loader.windows[0])
+win.show()
+app.exec_()
+
+while True:
+    #if clicked on welcome the the next window is loaded
+    if ui.clicked == 1:
+        if dbexists():
+            ui,win = loader.load(loader.windows[1])
+            win.show()
+            app.exec_()
+        else :
+            ui,win = loader.load(loader.windows[2])
+            win.show()
+            app.exec_()
+    else:
+        break
+
+    if ui.clicked == 1 :
+        if ui.validated :#add or dbexists()[unimplimented only for testing]: 
+        #for testing 
+        # SHOULD BE 
+        # REMOVED WHEN RUNNING
+            ui,win = loader.load(loader.windows[3])
+            win.show()
+            app.exec_()
+            break
+        else:
+            ui,win = loader.load(loader.windows[0])
+            win.show()
+            app.exec_()
+    else:
+        break
+
